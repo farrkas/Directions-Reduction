a518aefcd103c50c8b1aa23c216abea93f454152
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-16 22:25:14 +0100

    zle podmienia lementy tablicy arr

--- navigation.js
+++ navigation.js
@@ -1,3 +1,19 @@
-function dirReduc(arr){
-    
-  }
+function dirReduc(arr) {
+
+
+    arr.forEach((element) => { element.toUpperCase() });
+    //arr.forEach((element) =>{if (element!="NORTH" || element!="NORTH" || element!="EAST" || element!="WEST") console.log("Wpisano zły kierunek!"; break)});
+    var wartosci = arr.map((element) => {
+        if (element == "NORTH") return (1);
+        else if (element == "SOUTH") return (-1);
+        else if (element == "EAST") return (2);
+        else if (element == "WEST") return (-2);
+        //else {console.log("Wpisano zły kierunek!"); break;}
+
+
+    });
+    wartosci.forEach((element, i) => { if (element[i] == -element[i + 1] ) arr[i]=""; arr[i+1]="" });
+    console.log(arr);
+
+}
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
