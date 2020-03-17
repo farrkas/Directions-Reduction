b5e3ffafff31a83c4605ecabfac0bd67af3bc591
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-17 23:41:06 +0100

    posortowane

--- navigation.js
+++ navigation.js
@@ -2,34 +2,23 @@
 
 function dirReduc(arr) {
 
-
-    arr.forEach((element) => { element.toUpperCase() });
+    arr.forEach((element)=>String(element));
+    arr.forEach((arr) => { arr.toUpperCase() });
     //arr.forEach((element) =>{if (element!="NORTH" || element!="NORTH" || element!="EAST" || element!="WEST") console.log("Wpisano zły kierunek!"; break)});
     var wartosci = arr.map((element) => {
         if (element == "NORTH") return (1);
         else if (element == "SOUTH") return (-1);
         else if (element == "EAST") return (2);
         else if (element == "WEST") return (-2);
-        //else {console.log("Wpisano zły kierunek!"); break;}
+        //mozna zmienic na switch tylko musze przeczytac jak
 
 
     });
+    wartosci.sort();
 
-    var zmienione = [];
-    let i = 0;
-    let l = 0;
-    do {
-        if (wartosci[i] == -wartosci[i + 1]) {
 
-            i = i + 2;
-        }
-        else {
-            zmienione[l] = arr[i];
-            i++;
-            l++;
-        }
-    } while (i <= arr.lenght + 1);
-    console.log(zmienione);
+
+    console.log(wartosci);
 
 }
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
