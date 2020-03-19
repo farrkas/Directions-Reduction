96d146ed4b84b1e5fc8b926f18d187710b53f799
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-19 23:03:50 +0100

    udało się!

--- navigation.js
+++ navigation.js
@@ -1,15 +1,10 @@
 //https://www.codewars.com/kata/550f22f4d758534c1100025a/train/javascript
 
 function dirReduc(arr) {
-
-
     const upperArr = arr.map((element) => { return element.toUpperCase() });
-    //arr.forEach((element) =>{if (element!="NORTH" || element!="NORTH" || element!="EAST" || element!="WEST") console.log("Wpisano zły kierunek!"; break)});
 
     var wartosci = upperArr.map((element) => {
         switch (element) {
-
-
             case "NORTH": return (1); break;
             case "SOUTH": return (-1); break;
             case "EAST": return (2); break;
@@ -17,19 +12,31 @@ function dirReduc(arr) {
             default: console.log("Wpisaleś zły kierunek!");
         }
     });
-    //function porownanie(porownane) {
-    //return this;
-    var wynik = wartosci.map((el, i, array) => {
-        if (Math.abs(el) != Math.abs(array[i + 1])) return el; i + 2;
+
+    function porownanie(wart) {
+        let i = 0;
+        wynik = wartosci;
+        while (i <= wart.length - 1) {
+            if (Math.abs(wart[i]) == Math.abs(wart[i + 1])) { wynik.splice(i, 2); i++; }
+            else i = i + 2;
+        }
+        //wynik = wartosci;
+    }
+    porownanie(wartosci);
+    porownanie(wynik);
 
 
+    var koncowy = wartosci.map((element) => {
+        switch (element) {
+            case 1: return ("NORTH"); break;
+            case -1: return ("SOUTH"); break;
+            case 2: return ("EAST"); break;
+            case -2: return ("WEST"); break;
+            //default: ;
+        }
     });
-
-    //}
-    //wartosci.porownanie();
-
-
-    console.log(wynik);
-
+    console.log(koncowy);
 }
-dirReduc(["North", "South", "South", "East", "West", "North", "West"]);
+dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
+dirReduc(["NORTH", "WEST", "SOUTH", "EAST"]);
+dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"]);
