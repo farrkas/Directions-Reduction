b4d58bf0c54f88bf6e30166b77d28b3ab080b790
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-19 22:08:53 +0100

    a

--- navigation.js
+++ navigation.js
@@ -3,31 +3,33 @@
 function dirReduc(arr) {
 
 
-    arr.forEach((element) => { element.toUpperCase() });
+    const upperArr = arr.map((element) => { return element.toUpperCase() });
     //arr.forEach((element) =>{if (element!="NORTH" || element!="NORTH" || element!="EAST" || element!="WEST") console.log("Wpisano zły kierunek!"; break)});
-    var wartosci = arr.map((element) => {
-        if (element == "NORTH") return (1);
-        else if (element == "SOUTH") return (-1);
-        else if (element == "EAST") return (2);
-        else if (element == "WEST") return (-2);
-        //else {console.log("Wpisano zły kierunek!"); break;}
-    });
 
-    var wynik = [];
-    let l = 0;
-    wartosci.forEach((i, array) => {
+    var wartosci = upperArr.map((element) => {
+        switch (element) {
 
-        if (arr[i] + array[i + 1] != 0) {
-        wynik[l] = arr[i];
-        l++;
-        i=i+2;
+
+            case "NORTH": return (1); break;
+            case "SOUTH": return (-1); break;
+            case "EAST": return (2); break;
+            case "WEST": return (-2); break;
+            default: console.log("Wpisaleś zły kierunek!");
         }
-
-        else if ()
     });
+    //function porownanie(porownane) {
+    //return this;
+    var wynik = wartosci.map((el, i, array) => {
+        if (Math.abs(el) != Math.abs(array[i + 1])) return el; i + 2;
+
+
+    });
+
+    //}
+    //wartosci.porownanie();
 
 
     console.log(wynik);
 
 }
-dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
+dirReduc(["North", "South", "South", "East", "West", "North", "West"]);
